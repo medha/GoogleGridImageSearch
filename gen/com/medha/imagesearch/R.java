dc110717b52554c6de394010f3dfc629ab0f567e
Medha Ghatikesh <medha.ghati@gmail.com>
2013-11-19 17:55:08 -0800

    Styling

--- gen/com/medha/imagesearch/R.java
+++ gen/com/medha/imagesearch/R.java
@@ -9,11 +9,14 @@ package com.medha.imagesearch;
 
 public final class R {
     public static final class array {
-        public static final int color_filter_array=0x7f060001;
-        public static final int image_sizes_array=0x7f060000;
-        public static final int image_type_array=0x7f060002;
+        public static final int color_filter_array=0x7f070001;
+        public static final int image_sizes_array=0x7f070000;
+        public static final int image_type_array=0x7f070002;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int pressed_imagesearchexample=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -22,34 +25,35 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int pressed_background_imagesearchexample=0x7f020001;
     }
     public static final class id {
-        public static final int bSave=0x7f090013;
-        public static final int bSearch=0x7f090002;
-        public static final int etSearch=0x7f090001;
-        public static final int etSiteFilter=0x7f090011;
-        public static final int gvImages=0x7f090003;
-        public static final int image_search_settings=0x7f090014;
-        public static final int ivResult=0x7f090000;
-        public static final int spinColorFilter=0x7f09000b;
-        public static final int spinImageSize=0x7f090008;
-        public static final int spinImageType=0x7f09000e;
-        public static final int tableLayout1=0x7f090005;
-        public static final int tableRow0=0x7f090006;
-        public static final int tableRow1=0x7f090009;
-        public static final int tableRow2=0x7f09000c;
-        public static final int tableRow3=0x7f09000f;
-        public static final int tableRow4=0x7f090012;
-        public static final int textView1=0x7f090004;
-        public static final int tvColorFilter=0x7f09000a;
-        public static final int tvImageSize=0x7f090007;
-        public static final int tvImageType=0x7f09000d;
-        public static final int tvSiteFilter=0x7f090010;
+        public static final int bSave=0x7f0a0013;
+        public static final int bSearch=0x7f0a0002;
+        public static final int etSearch=0x7f0a0001;
+        public static final int etSiteFilter=0x7f0a0011;
+        public static final int gvImages=0x7f0a0003;
+        public static final int image_search_settings=0x7f0a0014;
+        public static final int ivResult=0x7f0a0000;
+        public static final int spinColorFilter=0x7f0a000b;
+        public static final int spinImageSize=0x7f0a0008;
+        public static final int spinImageType=0x7f0a000e;
+        public static final int tableLayout1=0x7f0a0005;
+        public static final int tableRow0=0x7f0a0006;
+        public static final int tableRow1=0x7f0a0009;
+        public static final int tableRow2=0x7f0a000c;
+        public static final int tableRow3=0x7f0a000f;
+        public static final int tableRow4=0x7f0a0012;
+        public static final int textView1=0x7f0a0004;
+        public static final int tvColorFilter=0x7f0a000a;
+        public static final int tvImageSize=0x7f0a0007;
+        public static final int tvImageType=0x7f0a000d;
+        public static final int tvSiteFilter=0x7f0a0010;
     }
     public static final class layout {
         public static final int activity_image_display=0x7f030000;
@@ -58,31 +62,31 @@ public final class R {
         public static final int simple_image_view=0x7f030003;
     }
     public static final class menu {
-        public static final int image_display=0x7f080000;
-        public static final int image_search=0x7f080001;
-        public static final int settings=0x7f080002;
+        public static final int image_display=0x7f090000;
+        public static final int image_search=0x7f090001;
+        public static final int settings=0x7f090002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int advanced_search_options=0x7f050008;
-        public static final int app_name=0x7f050000;
-        public static final int black=0x7f050010;
-        public static final int color_filter=0x7f05000b;
-        public static final int empty=0x7f050012;
-        public static final int face=0x7f050011;
-        public static final int hello_world=0x7f050006;
-        public static final int image_size=0x7f05000a;
-        public static final int image_type=0x7f05000c;
-        public static final int load_more=0x7f050013;
-        public static final int save=0x7f05000e;
-        public static final int search_Button=0x7f050002;
-        public static final int search_for_images=0x7f050003;
-        public static final int settings=0x7f050004;
-        public static final int site_filter=0x7f05000d;
-        public static final int small=0x7f05000f;
-        public static final int title_activity_image_display=0x7f050005;
-        public static final int title_activity_settings=0x7f050007;
-        public static final int www_example_com=0x7f050009;
+        public static final int action_settings=0x7f060001;
+        public static final int advanced_search_options=0x7f060008;
+        public static final int app_name=0x7f060000;
+        public static final int black=0x7f060010;
+        public static final int color_filter=0x7f06000b;
+        public static final int empty=0x7f060012;
+        public static final int face=0x7f060011;
+        public static final int hello_world=0x7f060006;
+        public static final int image_size=0x7f06000a;
+        public static final int image_type=0x7f06000c;
+        public static final int load_more=0x7f060013;
+        public static final int save=0x7f06000e;
+        public static final int search_Button=0x7f060002;
+        public static final int search_for_images=0x7f060003;
+        public static final int settings=0x7f060004;
+        public static final int site_filter=0x7f06000d;
+        public static final int small=0x7f06000f;
+        public static final int title_activity_image_display=0x7f060005;
+        public static final int title_activity_settings=0x7f060007;
+        public static final int www_example_com=0x7f060009;
     }
     public static final class style {
         /** 
@@ -106,10 +110,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
+        public static final int Theme_Panache=0x7f080002;
+        public static final int Theme_Panache_ActionBar=0x7f080003;
+        public static final int Theme_Panache_ActionBar_TitleTextStyle=0x7f080004;
     }
 }
